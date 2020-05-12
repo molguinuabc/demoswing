06968ba80ecc24ebacc370fd93567d8c487eb53e
molguin <molguin@uabc.edu.mx>
2020-05-12 13:35:20 -0700

    Agregue mas componentes a la vista y un mejor acomodo

--- src/demoguimenuswing/DemoGUIMenuSwing.java
+++ src/demoguimenuswing/DemoGUIMenuSwing.java
@@ -5,13 +5,20 @@
  */
 package demoguimenuswing;
 
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 /**
  *
@@ -26,8 +33,20 @@ public class DemoGUIMenuSwing {
         JFrame ventana = new JFrame("Super Juego Fake");
         ventana.setSize(800,600);
         ventana.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        ventana.setLayout(new java.awt.GridLayout(2, 1));
+        JPanel panelSuperior = new JPanel(new java.awt.FlowLayout(FlowLayout.CENTER,20,20));
+        JLabel etiqueta = new JLabel("Comando: ");
+        panelSuperior.add(etiqueta);
+        JTextField comando = new JTextField("ingresa comando",40);
+        panelSuperior.add(comando);
+        ventana.add(panelSuperior);
+        
+        JScrollPane sp = new JScrollPane();
         JTextArea areaDeTexto = new JTextArea("Soy el area de texto");
-        ventana.add(areaDeTexto);
+        areaDeTexto.setPreferredSize(new Dimension(800,600));
+        sp.setViewportView(areaDeTexto);
+        ventana.add(sp);
+        
         
         JMenuBar barraMenu = new JMenuBar();
         ventana.setJMenuBar(barraMenu);
